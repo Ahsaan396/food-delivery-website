08ef66be43f2dd6a6f2f5c93a53eddf8d04b455c
Ahsaan396 <ahsanulhakim285@gmail.com>
2025-08-30 22:08:36 +0600

    cart item increment and decrement working

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,15 +1,47 @@
-import { createContext } from "react";
+import { createContext, useState , useEffect} from "react";
 import { food_list } from "../assets/assets";
-export const StoreContext=createContext();
 
-const StoreContextProvider=({children})=>{
-    const contextValue={
-        food_list
+export const StoreContext = createContext();
+
+const StoreContextProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {children}
-        </StoreContext.Provider>
-    )
-}
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      if (prev[itemId] === 1) {
+        const newCart = { ...prev };
+        delete newCart[itemId];
+        return newCart;
+      }
+      return { ...prev, [itemId]: prev[itemId] - 1 };
+    });
+  };
+
+   useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
 export default StoreContextProvider;
