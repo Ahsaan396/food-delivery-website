80632ba7580fece5e5cfe560411ac19d6614a665
Ahsaan396 <ahsanulhakim285@gmail.com>
2025-09-03 00:41:38 +0600

    added calculation for total amount in cart section

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState , useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext();
@@ -25,23 +25,36 @@ const StoreContextProvider = ({ children }) => {
     });
   };
 
-   useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find(product => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
 
-  const contextValue = {
-    food_list,
-    cartItems,
-    setCartItems,
-    addToCart,
-    removeFromCart,
+  }
+
+    // useEffect(() => {
+    //   console.log(cartItems);
+    // }, [cartItems]);
+
+    const contextValue = {
+      food_list,
+      cartItems,
+      setCartItems,
+      addToCart,
+      removeFromCart,
+      getTotalCartAmount
+    };
+
+    return (
+      <StoreContext.Provider value={contextValue}>
+        {children}
+      </StoreContext.Provider>
+    );
   };
-
-  return (
-    <StoreContext.Provider value={contextValue}>
-      {children}
-    </StoreContext.Provider>
-  );
-};
 
 export default StoreContextProvider;
